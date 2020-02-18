84ed1c01828104f3ca2232523cdfc2034c24b84f
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-17 21:09:50 -0600

    Components

--- src/App.js
+++ src/App.js
@@ -2,21 +2,18 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Cart from './components/Cart'
+import Default from './components/Default'
+import Details from './components/Details'
+import Navbar from './components/Navbar'
+import Product from './components/Product'
+import ProductList from './components/ProductList'
 
 function App() {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-6">COLUMN1</div>
-        <div className="col-6">
-          <span>
-            <i className="fas fa-home" />
-            Home
-          </span>
-        </div>
-      </div>
-    </div>
+    <React.Fragment>
+      <Navbar/>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/ProductList.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Product from "./Product"
+
+export default class ProductList extends Component {
+render() {
+        return (
+            <h3>PRODUCTLIST COMP RUNNING!</h3>
+        )
+    }
+}
