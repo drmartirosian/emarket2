e747f7f7fd46357924512229d745b5f8fe8d2c09
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-19 00:05:22 -0600

    Hooking up products list

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../logo.svg'
-// import styled from 'styled-components'
+import styled from 'styled-components'
 import { ButtonContainer } from './Button'
 
 export default class Navbar extends Component {
     render() {
         return (
-            <nav className="navbar bg-primary  navbar-expand-sm navbar-dark px-sm-5">
+            <NavWrapper className="navbar navbar-expand-sm navbar-dark px-sm-5">
                 {/*
                     https://www.iconfinder.com/icons/1243689/call_phone_icon
                     Creative Commons (Attribution 3.0 Unported);
@@ -31,7 +31,15 @@ export default class Navbar extends Component {
                         </span>
                     </ButtonContainer>
                 </Link>
-            </nav>
+            </NavWrapper>
         );
     }
 }
+const NavWrapper = styled.nav `
+background: var(--mainBlue);
+.nav-link{
+    color:var(--mainWhite)!important;
+    font-size:1.3rem;
+    text-transform:capitalize!important;
+}
+`
