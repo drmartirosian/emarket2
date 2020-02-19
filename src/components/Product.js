2a994304cd9b4c648ffbef6882215f459c2deb39
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-19 00:51:29 -0600

    to cart btn logic, does nothing yet

--- src/components/Product.js
+++ src/components/Product.js
@@ -9,10 +9,26 @@ export default class Product extends Component {
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
                 <div className="card">
-                    <div className="img-container p-5" onClick={console.log("You clicked me (from Product.js)")}>
+                    <div 
+                        className="img-container p-5" 
+                        onClick={()=> console.log("You clicked me (from Product.js)")}>
                         <Link to="/details" >
                             <img src={img} alt="Product img" className="card-img-top"/>
                         </Link>
+                        <button 
+                            className="cart-btn"
+                            disabled={inCart?true:false}
+                            onClick={()=>{console.log("ADDED TO CART");
+                        }}>
+                            {inCart?(
+                                <p className="text-capitalize mb-0" disabled>
+                                    {" "}
+                                    in cart
+                                </p>
+                                ):(
+                                <i className="fas fa-cart-plus"/>
+                            )} +
+                        </button>
                     </div>
                 </div>
             </ProductWrapper>
