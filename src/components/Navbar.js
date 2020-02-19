1f8fab9b8873e74af4efca42692ee2f32fc928cb
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-18 23:15:26 -0600

    Maded button comp

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import logo from '../logo.svg'
-import styled from 'styled-components'
-
+// import styled from 'styled-components'
+import { ButtonContainer } from './Button'
 
 export default class Navbar extends Component {
     render() {
@@ -35,22 +35,3 @@ export default class Navbar extends Component {
         );
     }
 }
-
-const ButtonContainer = styled.button`
-text-transform:capitalize;
-font-size: 1.43rem;
-background: transparent;
-border: 0.05rem solid var(--lightBlue);
-color:var(--lightBlue);
-border-radius: 0.5rem;
-padding: 0.2rem 0.5rem;
-margin: 0.2rem 0.2rem 0;
-transition: all 0.5s ease-in-out;
-&:hover{
-    background:var(--lightBlue);
-    color:var(--mainBlue);
-}
-&:focus{
-    outline:none;
-}
-`
