495c64605640659151190e44efc6f02b0585bc32
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-23 01:11:13 -0600

    Cart works

--- src/context.js
+++ src/context.js
@@ -6,7 +6,7 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: storeProducts,
+        cart: [],
         modelOpen: false,
         modalProduct: detailProduct,
         cartSubTotal: 0,
