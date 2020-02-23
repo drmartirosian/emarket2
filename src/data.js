4c0b6ca5f49551e89a2fb51ec1464c165f61edbd
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-22 23:37:47 -0600

    Starting cart

--- src/data.js
+++ src/data.js
@@ -99,7 +99,103 @@ export const storeProducts = [
     inCart: false,
     count: 0,
     total: 0
-  }
+  },
+  {
+    id: 1,
+    title: "Sock - Used",
+    img: "img/product-1.png",
+    price: 10,
+    company: "iSocks",
+    info:
+      "For those wondering how their socks keep disapearing... Hey -- What's that over there! Cool! Now buy this sock! Totally can bleach those stains (and mold) right out! BUY-BUY-BUY!!!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 2,
+    title: "Ring of Power",
+    img: "img/product-2.png",
+    price: 1,
+    company: "Sauron",
+    info:
+      "Useless trash... Says something about one ring to rull them all, or what have you... Huh... You know on second thought, it's not for sale... Hey! Hands off! IT'S MY... my... PRECIOUS...",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 3,
+    title: "Sandwich - Half Eaten",
+    img: "img/product-3.png",
+    price: 8,
+    company: "Co-Worker",
+    info:
+      "I keep finding food in the fridge at work. Lucky me!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 4,
+    title: "Rock - House Trained",
+    img: "img/product-4.png",
+    price: 50,
+    company: "Pet's 4EVER",
+    info:
+      "Loyal, trained, ready to roll. Next generation pet ready to lava you! Boulder than a dog, unlike cats will take nothing for granite. Introducing: the Rock.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 5,
+    title: "Noodle",
+    img: "img/product-5.png",
+    price: 0,
+    company: "????",
+    info:
+      "Whatever...",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 6,
+    title: "Robo-Dog!",
+    img: "img/product-6.png",
+    price: 17000,
+    company: "K9 Inc",
+    info:
+      "They said he would never walk again after the accident. We rebuilt him. Better. Faster. Stronger.",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 7,
+    title: "Evil Pickle",
+    img: "img/product-7.png",
+    price: 6.66,
+    company: "The Cucumber Company",
+    info:
+      "Whispers... It whispers to me... E-Evil! It is EVIL! AAHHHHH!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
+  {
+    id: 8,
+    title: "Magic Potion",
+    img: "img/product-8.png",
+    price: 200,
+    company: "Random Hobo Inc.",
+    info:
+      "That wizard under the bridge gave me this magic potion for 50 bucks. Said he makes them behind the bushes over there. Didn't say what it does, though... Must be magic, indeed!",
+    inCart: false,
+    count: 0,
+    total: 0
+  },
 ];
 
 export const detailProduct = {
