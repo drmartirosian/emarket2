bb9f3849db1b4484d166ee30723449a46c7eaaa8
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2020-02-23 02:39:20 -0600

    Fixed bug

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import {Switch, Route} from 'react-router-dom'
-// import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
 import ProductList from './components/ProductList'
 import Details from './components/Details'
-import Cart from './components/cart'
+import Cart from './components/cart/Cart'
 import Default from './components/Default'
 import Model from './components/Model'
 
